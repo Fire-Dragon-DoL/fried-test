3ccb8ba2c851611b3eb541a2c80d48aea8af0076
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2017-11-18 03:16:00 -0800

    Fixed module extending rake dsl

--- lib/fried/test/rake.rb
+++ lib/fried/test/rake.rb
@@ -3,15 +3,19 @@ require "fried/core"
 
 module Fried::Test
   module Rake
-    extend ::Rake::DSL
+    def self.included(klass)
+      klass.class_eval do
+        include ::Rake::DSL
+      end
 
-    task :test do
-      lib = File.expand_path("../../../", __FILE__)
-      $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+      task :test do
+        lib = File.expand_path("../../../", __FILE__)
+        $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-      require "fried/test"
+        require "fried/test"
 
-      Fried::Test::CLI.()
+        Fried::Test::CLI.()
+      end
     end
   end
 end
