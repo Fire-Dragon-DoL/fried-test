a28e71d8c42f6cddc199eb3e3fd98f32a5f22e12
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2017-11-17 23:03:35 -0800

    Refactored GetTestDirectory tests

--- test/automated/get_test_directory_test.rb
+++ test/automated/get_test_directory_test.rb
@@ -1,14 +1,19 @@
 require "test_helper"
 require "fried/test/get_test_directory"
+require "fried/test/directory/current_working_directory"
 
 class GetTestDirectoryTest < Minitest::Spec
+  Directory = Fried::Test::Directory
   GetTestDirectory = Fried::Test::GetTestDirectory
+  CurrentWorkingDirectory = Directory::CurrentWorkingDirectory::Substitute
 
-  it "doesn't do anything" do
+  it "appends /test to current working directory" do
+    current_working_directory = CurrentWorkingDirectory.new("/tmp/foo")
     get_test_directory = GetTestDirectory.new
+    get_test_directory.current_working_directory = current_working_directory
 
     path = get_test_directory.()
 
-    assert path.to_s == "/dev/null/test"
+    assert path.to_s == "/tmp/foo/test"
   end
 end
