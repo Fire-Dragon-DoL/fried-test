9ffb277dd2d80429ed9a21bdfaa27e9b10598bfa
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2017-11-17 21:43:05 -0800

    Added tests for get files by pattern

---
+++ test/automated/directory/get_files_by_pattern_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+require "fried/test/directory/get_files_by_pattern"
+
+class GetFilesByPatternTest < Minitest::Spec
+  GetFilesByPattern = Fried::Test::Directory::GetFilesByPattern
+
+  it "is /dev/null" do
+    get_files_by_pattern = GetFilesByPattern.new
+
+    files = get_files_by_pattern.("/dev/null/**/*")
+
+    assert files.empty?
+  end
+end
