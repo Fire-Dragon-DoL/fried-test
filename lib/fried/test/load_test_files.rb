dbf660e320696af7ff0d163581a9cdb79e8856b0
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2017-11-28 01:58:44 -0800

    Added possibility to run single test file

--- lib/fried/test/load_test_files.rb
+++ lib/fried/test/load_test_files.rb
@@ -1,5 +1,6 @@
 require "fried/core"
 require "fried/test/get_test_files"
+require "fried/test/get_single_test_file"
 require "fried/test/options"
 
 module Fried::Test
@@ -8,21 +9,24 @@ module Fried::Test
     class Substitute
       include ::Fried::Test::Telemetry
 
-      def call
-        record :call
+      def call(options = Options::Default)
+        record :call, options
         nil
       end
     end
 
     attr_accessor :get_test_files
+    attr_accessor :get_single_test_file
 
     def initialize
       @get_test_files = GetTestFiles.new
+      @get_single_test_file = GetSingleTestFile.new
     end
 
     def self.build
       new.tap do |instance|
         instance.get_test_files = GetTestFiles.build
+        instance.get_single_test_file = GetSingleTestFile.build
       end
     end
 
